57a56f82c552957f815f91e65ee9bef3368c70d5
Guilherme Barros <guilhermecassimiro425@gmail.com>
2025-09-09 14:33:04 -0300

    tupla de filmes adicionada

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 from functions import *
+from classes import *
+
+filme1 = Film('01', 'Titanic' 'Romance' '3h14min' 
+'02,', 'Laranja Mecânica', 'Comédia', '2h16min'
+'03', 'Clube da Luta', 'Ação', '2h19'
+'04', 'Coringa', 'Drama', '2h2min'
+'05', 'The Batman', 'Ação', '2h56min'
+'06', 'Wicked', 'Musical', '2h40min'
+'07', 'Forrest Gump: O Contador de Histórias', 'Comédia', '2h22min'
+'08', 'Taxi Driver', 'Suspense', '1h54min')
+lista_de_filmes = []
+
+
 
 main_menu()
-
