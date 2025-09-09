438e093d6fb7800b8ab63d0c81817e3dbd3bc95c
PedrohPedrassolli03 <pedrohpedrassolli03@gmail.com>
2025-09-09 15:42:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from classes import *
 
 filme1 = Film('01', 'Titanic' 'Romance' '3h14min')
 filme2 = Film('02,', 'Laranja Mecânica', 'Comédia', '2h16min')
-filme3 = Film('03', 'Clube da Luta', 'Ação', '2h19')
+filme3 = Film('03', 'Clube da Luta', 'Ação', '2h19min')
 filme4 = Film('04', 'Coringa', 'Drama', '2h2min')
 filme5 = Film('05', 'The Batman', 'Ação', '2h56min')
 filme6 = Film('06', 'Wicked', 'Musical', '2h40min')
