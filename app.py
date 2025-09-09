be533e773771cbcb4fda48a4a4fea0d8b17a3cde
Guilherme Barros <guilhermecassimiro425@gmail.com>
2025-09-09 14:42:08 -0300

    correção dos filmes

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from functions import *
 from classes import *
 
-filme1 = Film('01', 'Titanic' 'Romance' '3h14min' 
-'02,', 'Laranja Mecânica', 'Comédia', '2h16min'
-'03', 'Clube da Luta', 'Ação', '2h19'
-'04', 'Coringa', 'Drama', '2h2min'
-'05', 'The Batman', 'Ação', '2h56min'
-'06', 'Wicked', 'Musical', '2h40min'
-'07', 'Forrest Gump: O Contador de Histórias', 'Comédia', '2h22min'
-'08', 'Taxi Driver', 'Suspense', '1h54min')
+filme1 = Film('01', 'Titanic' 'Romance' '3h14min')
+filme2 = Film('02,', 'Laranja Mecânica', 'Comédia', '2h16min')
+filme3 = Film('03', 'Clube da Luta', 'Ação', '2h19')
+filme4 = Film('04', 'Coringa', 'Drama', '2h2min')
+filme5 = Film('05', 'The Batman', 'Ação', '2h56min')
+filme6 = Film('06', 'Wicked', 'Musical', '2h40min')
+filme7 = Film('07', 'Forrest Gump: O Contador de Histórias', 'Comédia', '2h22min')
+filme8 = Film('08', 'Taxi Driver', 'Suspense', '1h54min')
 lista_de_filmes = []
 
 
