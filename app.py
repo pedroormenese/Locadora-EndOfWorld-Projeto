27c3b1e73f3596e24adc5fec348c382e145e71d4
Pedro F Ormenese <pedro.f.ormenese@gmail.com>
2025-09-09 16:00:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
 from functions import *
 from classes import *
 
-filme1 = Film('01', 'Titanic' 'Romance' '3h14min')
-filme2 = Film('02,', 'Laranja Mecânica', 'Comédia', '2h16min')
-filme3 = Film('03', 'Clube da Luta', 'Ação', '2h19min')
-filme4 = Film('04', 'Coringa', 'Drama', '2h2min')
-filme5 = Film('05', 'The Batman', 'Ação', '2h56min')
-filme6 = Film('06', 'Wicked', 'Musical', '2h40min')
-filme7 = Film('07', 'Forrest Gump: O Contador de Histórias', 'Comédia', '2h22min')
-filme8 = Film('08', 'Taxi Driver', 'Suspense', '1h54min')
-lista_de_filmes = []
-
-
-
 main_menu()
