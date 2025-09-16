dc52a13475e5fa475fd8dad0a88b65039a166b25
Pedro F Ormenese <pedro.f.ormenese@gmail.com>
2025-09-16 07:47:12 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -7,7 +7,6 @@ locadora.cadastrarItem(Filme(1, "Meus 84m", "Terror Psicológico", "1h58min"))
 locadora.cadastrarItem(Filme(2, "Fuja", "Terror", "1h30min"))
 locadora.cadastrarItem(Filme(3, "Meu ano em Oxford", "Drama", "1h52min"))
 locadora.cadastrarItem(Filme(4, "Até que as cores acabem", "Drama", "1h58min"))
-
 locadora.cadastrarItem(Jogo(5, "007 Golden Eye", "Nintendo 64", "+12"))
 locadora.cadastrarItem(Jogo(6, "Sonic Adventures 2", "Dreamcast", "+10"))
 locadora.cadastrarItem(Jogo(7, "Super Mario Bros 3", "NES", "Livre"))
@@ -16,6 +15,7 @@ locadora.cadastrarItem(Jogo(8, "Alex Kidd in the Micacle World", "Master System"
 
 # --------------------------------------------------- Cadastrar
 def cadastro():
+    os.system("cls")
     nome = input("Insira seu nome: ")
     cpf = int(input("Insira seu CPF (somente números): "))
     cliente = Cliente(nome, cpf)
@@ -25,21 +25,28 @@ def cadastro():
 # --------------------------------------------------- Login
 
 def login():
+    os.system("cls")
     nome = input("Insira seu nome: ")
     cpf = int(input("Insira seu CPF (somente números): "))
-    for cliente in locadora.clientes:
-        if nome == cliente.nome and cpf == cliente.cpf:
-            print("Login efetuado com sucesso!")
-            return cliente
-        else:
-            print("O usuário e/ou CPF não existem ou não foram encontrados. Certifique-se de que os dados estão corretos.")
-            os.system("pause")
+    if not locadora.clientes:
+        print("O usuário e/ou CPF não existem ou não foram encontrados. Certifique-se de que os dados estão corretos.")
+        os.system("pause")
+    else:
+        for cliente in locadora.clientes:
+            if nome == cliente.nome and cpf == cliente.cpf:
+                print("Login efetuado com sucesso!")
+                os.system("pause")
+                return cliente
+            else:
+                print("O usuário e/ou CPF não existem ou não foram encontrados. Certifique-se de que os dados estão corretos.")
+                os.system("pause")
 
 # --------------------------------------------------- Menu Principal
 
 def mainmenu(cliente: Cliente):
     while True:
         try:
+            os.system("cls")
             opcoes = ["1. Locar item", "2. Devolver item", "3. Cadastrar item", "4. Ver itens alugados", "5. Sair"]
             print(locadora.nome)
             print("\n")
@@ -50,6 +57,7 @@ def mainmenu(cliente: Cliente):
             match i:
 
                 case 1:
+                    os.system("cls")
                     locadora.listItens()
                     item_codigo = int(input("Digite o código do item que você deseja locar: "))
                     item_encontrado = False
@@ -57,9 +65,11 @@ def mainmenu(cliente: Cliente):
                         if item_codigo == item.code:
                             cliente.locarProduto(item)
                             item_encontrado = True
+                            os.system("pause")
                             break
                     if not item_encontrado:
                         print("O produto não existe ou não foi encontrado")
+                        os.system("pause")
 
                 case 2:
                     item_devolver = False
@@ -69,8 +79,10 @@ def mainmenu(cliente: Cliente):
                        if item in cliente.itensLocados and item_codigo == item.code:
                            cliente.devolverProduto(item)
                            item_devolver = True
+                           os.system("pause")
                     if not item_devolver:
                         print("O item não foi encontrado ou você não o alugou")
+                        os.system("pause")
 
                 case 3:
                     opcoes_cadastro = ["1. Filme", "2. Jogo"]
@@ -94,8 +106,10 @@ def mainmenu(cliente: Cliente):
                             jogo = Jogo(code, nome_jogo, plataforma, class_idade)
                             locadora.cadastrarItem(jogo)
                 case 4:
+                    os.system("cls")
                     print("Itens alugados")
                     cliente.listItems()
+                    os.system("pause")
                 case 5:
                     break
         except Exception as e:
@@ -120,7 +134,6 @@ def startmenu():
                     mainmenu(cliente)
                 case 2:
                     cliente = login()
-                    os.system("cls")
                     if cliente is not None:
                         mainmenu(cliente)
                 case 3:
